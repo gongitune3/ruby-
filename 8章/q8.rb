01491b0d2d2e6a0b8d4bf601fede35bd11380373
gongitune14 <inoseshouhei@inoseshouheinoMacBook-Air.local>
2020-05-05 00:29:11 +0900

    Can't understand

---
+++ 8章/q8.rb
@@ -0,0 +1,20 @@
+puts "計算を始めます"
+puts "2つの値を入力してください"
+
+h = gets.to_i
+v = gets.to_i
+
+puts "計算結果を出力します"
+
+puts "h * v = #{h * v}"
+
+puts "計算を終了します"
+
+puts "計算を始めます"
+puts "何回繰り返しますか？"
+
+input = gets.to_i
+
+i = 1
+
+while i <= input do 
